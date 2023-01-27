74812e7deeeb67a892394da32ca6ed1f0ce17d53
kirtanchandak <themarketingkirtan@gmail.com>
2023-01-27 14:26:58 +0530

    database connected

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 app.set("view engine", "ejs");
-app.listen(3000);
+
+const dbURI = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cornellblog.4c5hnap.mongodb.net/cornell?retryWrites=true&w=majority`;
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //middlwares & static files -
 app.use(morgan("dev"));
@@ -29,3 +40,27 @@ app.get("/blogs/create", (req, res) => {
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const blogSchema = new Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true,
+//     },
+//     snippet: {
+//       type: String,
+//       required: true,
+//     },
+//     body: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// const Blog = mongoose.model("Blog", blogSchema);
+// module.exports = Blog;
